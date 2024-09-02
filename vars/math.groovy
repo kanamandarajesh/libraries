680a6480741c1361a4983bce32a8c47af02aad22
kanamandarajesh <108647030+kanamandarajesh@users.noreply.github.com>
2024-09-02 14:42:56 +0530

    Update math.groovy

--- vars/math.groovy
+++ vars/math.groovy
@@ -2,3 +2,15 @@ def sum(a,b)
 {   
   echo "The sum of ${a} and ${b} is ${a+b}"
 }  
+def sum(a,b)
+{   
+  echo "The sum of ${a} and ${b} is ${a-b}"
+}  
+def sum(a,b)
+{   
+  echo "The sum of ${a} and ${b} is ${a*b}"
+}  
+def sum(a,b)
+{   
+  echo "The sum of ${a} and ${b} is ${a%b}"
+}  
