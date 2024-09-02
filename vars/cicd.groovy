b744dc3171f2631ff6a8cb8881c44191d730528a
kanamandarajesh <108647030+kanamandarajesh@users.noreply.github.com>
2024-09-02 16:19:17 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -8,5 +8,5 @@ def newMaven()
 }   
 def newDeploy()
 {
-   deploy adapters: [tomcat7(credentialsId: '163a1995-8583-453d-8c5d-c57f0ae8bafe', path: '', url: 'http://${ip}')], contextPath: '${testapp}', war: '**/*.war'
+   sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war root@${ip}:/var/lib/tomcat/webapps/${context}.war"    
 }   
