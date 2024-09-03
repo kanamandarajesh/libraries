6636b3d0dcdacff073589c2875f8ee21d0e0bcdc
kanamandarajesh <108647030+kanamandarajesh@users.noreply.github.com>
2024-09-03 14:40:21 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -6,6 +6,6 @@ def newMaven() {
    sh 'mvn package'
 }
 
-def newDeploy() {
-deploy adapters: [tomcat7(credentialsId: '163a1995-8583-453d-8c5d-c57f0ae8bafe', path: '', url: 'http://192.168.199.137:8080')], contextPath: 'TestApp', war: '**/*.war'
-}    
+def newDeploy(ip,app) {
+deploy adapters: [tomcat7(credentialsId: '163a1995-8583-453d-8c5d-c57f0ae8bafe', path: '', url: "${ip}")], contextPath: "${app}", war: '**/*.war'
+}
