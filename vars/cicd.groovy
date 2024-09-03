cfca17c97d4dcb7233035c9c2c69677471b8c965
kanamandarajesh <108647030+kanamandarajesh@users.noreply.github.com>
2024-09-03 14:14:30 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -7,4 +7,5 @@ def newMaven() {
 }
 
 def newDeploy() {
-deploy adapters: [tomcat7(credentialsId: '163a1995-8583-453d-8c5d-c57f0ae8bafe', path: '', url: 'http://192.168.199.137:8080')], contextPath: 'TestApp', war: '**/*.war'}    
+deploy adapters: [tomcat7(credentialsId: '163a1995-8583-453d-8c5d-c57f0ae8bafe', path: '', url: 'http://192.168.199.137:8080')], contextPath: 'TestApp', war: '**/*.war'
+}    
