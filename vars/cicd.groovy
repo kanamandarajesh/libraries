421ef85459a6a124357f82bbb9e318bffe0abd93
kanamandarajesh <108647030+kanamandarajesh@users.noreply.github.com>
2024-09-03 13:38:02 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -7,5 +7,13 @@ def newMaven() {
 }
 
 def newDeploy(String jobname, String ip, String context) {
-    sh "scp -o StrictHostKeyChecking=no /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war root@${ip}:/var/lib/tomcat/webapps/${context}.war"
+    deploy adapters: [
+        tomcat7(
+            credentialsId: '163a1995-8583-453d-8c5d-c57f0ae8bafe', 
+            path: '', 
+            url: "http://${ip}:8080"
+        )
+    ], 
+    contextPath: context, 
+    war: "**/${jobname}/target/*.war"
 }
