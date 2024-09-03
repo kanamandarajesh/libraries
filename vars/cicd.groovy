4534db954f75e6ab1fa287baceedfcd11a54939f
kanamandarajesh <108647030+kanamandarajesh@users.noreply.github.com>
2024-09-03 15:07:30 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,11 +1,14 @@
 def newGit(String repo) {
-   git "${repo}"
+    git "${repo}"
 }
 
 def newMaven() {
-   sh 'mvn package'
+    sh 'mvn package'
 }
 
 def newDeploy(ip,app) {
-deploy adapters: [tomcat7(credentialsId: '163a1995-8583-453d-8c5d-c57f0ae8bafe', path: '', url: "${ip}")], contextPath: "${app}", war: '**/*.war'
+    deploy adapters: [tomcat7(credentialsId: '163a1995-8583-453d-8c5d-c57f0ae8bafe', path: '', url: "${ip}")], contextPath: "${app}", war: '**/*.war'
+}
+def runSelenium() {
+    sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
 }
