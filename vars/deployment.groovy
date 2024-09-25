6c88a653e46c7e8e44365ab6a43869f91d589011
kanamandarajesh <108647030+kanamandarajesh@users.noreply.github.com>
2024-09-26 02:17:22 +0530

    Update deployment.groovy

--- vars/deployment.groovy
+++ vars/deployment.groovy
@@ -1,9 +1,49 @@
-def newClone(string repo) {
-    git ${git_url}
+def newClone(String repo) {
+    git "${git_url}"
 }  
 def newBuild() {
     sh 'mvn clean package fabric8:build'
 }
-def newDeploy () {
-    sh 'kubectl apply -f deploy.yml'
+def newDeploy(user, ip) {
+    sh 'ssh -i StrictsshKey=no "${user}"@"${ip}" /root/kubernetes/kubectl apply -f deploy.yml'
+}    
+
+
+@Library(my_shared_library)_
+pipeline
+{
+    agent any
+    stages
+    {
+        stage('clone')
+        {
+            steps
+            {
+                script
+                {
+                    cicd.newClone("git_url")
+                }
+            }   
+        }
+        stage('build image')
+        {
+            steps
+            {
+                script
+                {
+                    cicd.newBuild()
+                }    
+            }   
+        }
+        stage('deploy')
+        {
+            steps
+            {
+                script
+                {
+                    cicd.newDeploy("root", "192.168.199.138")
+                }   
+            }   
+        }    
+    }
 }    
