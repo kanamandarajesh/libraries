4744de5c973b2b012a91400570530cd75c0066cf
kanamandarajesh <108647030+kanamandarajesh@users.noreply.github.com>
2024-09-26 01:55:02 +0530

    Update deployment.groovy

--- vars/deployment.groovy
+++ vars/deployment.groovy
@@ -1,3 +1,9 @@
-def newBuild() {
-    ci
+def newClone(string repo) {
+    git ${git_url}
 }  
+def newBuild() {
+    sh 'mvn clean package fabric8:build'
+}
+def newDeploy () {
+    sh 'kubectl apply -f deploy.yml'
+}    
